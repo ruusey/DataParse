6ce4f21cddb4c2afd2ff0e47988d00ea723110cc
Robert Usey <ruusey@gmail.com>
2019-09-19 09:23:30 -0400

    Latest

--- DataParse/src/com/http/HttpUtils.java
+++ DataParse/src/com/http/HttpUtils.java
@@ -10,15 +10,19 @@ public class HttpUtils {
 	private static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0";
 	public static String post(String url, String content) {
 		Post post = Http.post(url, content)
-				.header("User-Agent", USER_AGENT)
-				.header("Accept", "application/json")
-                .header("Content-Type", "application/json")
-                .header("Aceept-Language", "en-US,en;q=0.5");
+						.header("User-Agent", USER_AGENT)
+						.header("Accept", "application/json")
+						.header("Content-Type", "application/json")
+						.header("Aceept-Language", "en-US,en;q=0.5");
 		return post.text();
 
 	}
 	public static String get(String url, Map<String,String> values) {
-		Get get = Http.get(url);
+		Get get = 	Http.get(url)
+						.header("User-Agent", USER_AGENT)
+						.header("Accept", "application/json")
+						.header("Content-Type", "application/json")
+						.header("Aceept-Language", "en-US,en;q=0.5");
 		return get.text();
 	}
 }
